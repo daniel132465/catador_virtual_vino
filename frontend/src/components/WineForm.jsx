c159864b9b4401cfe34caa46ea934fd542c9c9fb
Daniel Catari <daniel.catari@example.com>
2025-11-27 03:11:21 -0400

    version 6.3.1 finalizado

--- frontend/src/components/WineForm.jsx
+++ frontend/src/components/WineForm.jsx
@@ -75,7 +75,8 @@ const WineForm = ({ isActive }) => {
                 alcohol: formData.alcohol
             };
 
-            const response = await fetch('http://127.0.0.1:8000/predict', {
+            const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+            const response = await fetch(`${API_URL}/predict`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
